a295dd3cca376e67f1e9ce0493c5669b3a4c7e84
Andrei Cristof <andreimcristof@gmail.com>
2022-01-06 21:46:44 +0100

    end

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,22 +1,23 @@
-import {useRouter} from 'next/router';
-import { getFilteredEvents } from '../../dummy-data';
-import EventList from '../../components/events/event-list';
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
 
-const FilteredEvents =() => {
-    const router = useRouter();
-    const filterData = router.query.slug;
-    if(!filterData) return <p className='center'>Loading...</p>
+const FilteredEvents = () => {
+  const router = useRouter();
+  const filterData = router.query.slug;
+  if (!filterData) return <p className="center">Loading...</p>;
 
-    const filteredYear = filterData[0];
-    const filteredMonth = filterData[1];
-    const year = +filteredYear;
-    const month = +filteredMonth;
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+  const year = +filteredYear;
+  const month = +filteredMonth;
 
-    if(isNaN(year) || isNaN(month)) return <p>Invalid filter, please adjust your values.</p>
+  if (isNaN(year) || isNaN(month))
+    return <p>Invalid filter, please adjust your values.</p>;
 
-    const events = getFilteredEvents({ year, month })
-    if (!events || events.length === 0) return <p>No events found.</p>
-    
-    return <EventList events={events} />
-}
+  const events = getFilteredEvents({ year, month });
+  if (!events || events.length === 0) return <p>No events found.</p>;
+
+  return <EventList events={events} />;
+};
 export default FilteredEvents;
